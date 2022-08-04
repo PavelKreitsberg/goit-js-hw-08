ba62dcee27521a945e95ef4488256b17dd3ed11a
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-08-04 22:12:52 +0300

    Solve task1 and task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery')
+}
+
+const createImgListMarkup = (arr) =>
+    arr.map(value =>
+        `<a class="gallery__item" href="${value.original}">
+            <img class="gallery__image" src="${value.preview}" alt="${value.description}" />
+        </a>`
+    ).join('');
+
+
+refs.gallery.insertAdjacentHTML('afterbegin', createImgListMarkup(galleryItems))
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const TIME = "videoplayer-current-time";   
+
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem(TIME, data.seconds.toString())
+}, 1000));
+    
+player.setCurrentTime(localStorage.getItem(TIME));
