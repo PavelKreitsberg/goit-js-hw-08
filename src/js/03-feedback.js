a3f37a0d96305e14f7d93644f964a4f19605cfef
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-08-07 14:08:30 +0300

    Solve task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+const throttle = require('lodash.throttle');
+
+refs = {
+    key: "feedback-form-state",
+    form: document.querySelector('.feedback-form'),
+    formInput: document.querySelector('.feedback-form input'),
+    formTextarea: document.querySelector('.feedback-form textarea'),
+    formBtnSubmit: document.querySelector('.feedback-form button'),
+}
+
+const userData = {
+    email: '',
+    massage: '',
+}
+
+refs.formInput.addEventListener('input', (e) => {
+
+    userData.email = e.currentTarget.value
+
+    localStorage.setItem(refs.key, JSON.stringify(userData));
+})
+
+refs.formTextarea.addEventListener('input', (e) => {
+
+    userData.massage = e.currentTarget.value
+
+    localStorage.setItem(refs.key, JSON.stringify(userData));
+})
+
+refs.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(refs.key)));
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem(refs.key);
+})
+
+const populateFormFields = () => {
+    if (localStorage.getItem(refs.key)) {
+        refs.formInput.value = JSON.parse(localStorage.getItem(refs.key)).email;
+
+        refs.formTextarea.value = JSON.parse(localStorage.getItem(refs.key)).massage;
+    }
+}
+
+populateFormFields();
+
+
+
+
+
 
